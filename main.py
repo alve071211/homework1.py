8eb106da9cfec1a96259270e660399cafd68aed8
alve071211 <alve071211@gmail.com>
2024-09-10 16:40:15 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+example = "водогрязеторфопарафинолечение"
+print(example[0])
+print(example[-1])
+first_half = example[:len(example)//2]
+second_half = example[len(example)//2:]
+print(second_half)
+print(example[::-1])
+print(example[1::2])
